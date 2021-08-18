5b1da4f8771fdbe9434e915b3f8bd38b7e305cb4
Łukasz Nycz <lnycz96@gmail.com>
2021-08-18 14:48:43 +0200

    tableView and model

--- Notes-App-Clone/ViewController.swift
+++ Notes-App-Clone/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class noteModel: Codable {
+    var noteTitle: String
+    var noteBody: String
+    
+    init(noteTitle: String, noteBody: String) {
+        self.noteTitle = noteTitle
+        self.noteBody = noteBody
+    }
+}
+
+
+class ViewController: UITableViewController {
+    
+    var noteModel: [noteModel]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNote))
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return noteModel.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var isNoteModel = noteModel[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = isNoteModel.noteTitle
+        return cell
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    @objc func addNote() {
+        
+    }
 
 }
 
