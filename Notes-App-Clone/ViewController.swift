d94bb0957c47e048c313904299089fbbc416e53e
Łukasz Nycz <lnycz96@gmail.com>
2021-08-18 12:55:10 +0200

    Initial Commit

---
+++ Notes-App-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes-App-Clone
+//
+//  Created by Łukasz Nycz on 18/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
